090eb103e54a676851c8a6d72a260d2471717216
shivam patel <shivampatelpatel2005@gmail.com>
2026-02-26 14:13:18 +0530

    Fixed Dashboard import case issue

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./Pages/Dashboard";
 import Login from "./Pages/Login";
 import PublicRoute from './Routes/PublicRoute';
 import PrivateRoute from './Routes/PrivateRoute';
